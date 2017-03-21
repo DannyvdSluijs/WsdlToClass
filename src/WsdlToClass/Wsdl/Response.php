d3909121544a338339921886c7662caba6ca79f2
Danny van der Sluijs <dvandersluijs@monitorlinq.com>
2017-03-21 21:44:22 +0100

    Add corrections,  unit test.

--- src/WsdlToClass/Wsdl/Response.php
+++ src/WsdlToClass/Wsdl/Response.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * wsdlToClass
+ * WsdlToClass
  *
- * PHP Version 5.3
+ * PHP Version 5.6
  *
- * @copyright 2015 Danny van der Sluijs <dammy.vandersluijs@icloud.com>
+ * @copyright 2015 Danny van der Sluijs <danny.vandersluijs@icloud.com>
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU-GPL
  * @link      http://dannyvandersluijs.nl
  */
---
+++ tests/src/WsdlToClass/Command/GenerateCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * WsdlToClass
+ *
+ * PHP Version 5.6
+ *
+ * @copyright 2015 Danny van der Sluijs <danny.vandersluijs@icloud.com>
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU-GPL
+ * @link      http://dannyvandersluijs.nl
+ */
+
+namespace WsdlToClassTest\Commands;
+
+use WsdlToClass\Commands\GenerateCommand;
+
+class GenerateCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \WsdlToClass\Commands\GenerateCommand::configure()
+     * @covers \WsdlToClass\Commands\GenerateCommand::execute()
+     */
+    public function testExecute()
+    {
+        $command = new GenerateCommand();
+        $input = $this->createMock('Symfony\Component\Console\Input\InputInterface');
+        $output = $this->createMock('Symfony\Component\Console\Output\OutputInterface');
+
+        $input->expects($this->atLeastOnce())
+            ->method('getOption')
+            ->willReturnMap([
+                ['destination', sys_get_temp_dir()],
+                ['namespace', 'DannyvdSluijs\WsdlToClass\PhpUnit']
+            ]);
+        $input->expects($this->atLeastOnce())
+            ->method('getArgument')
+            ->willReturnMap([
+                ['wsdl', 'http://ws.cdyne.com/ip2geo/ip2geo.asmx?WSDL']
+            ]);
+
+        $command->run($input, $output);
+    }
+}
