d69cbe5de09e7c2c6d378a0d1391de82f892125c
Danny van der Sluijs <dvandersluijs@monitorlinq.com>
2017-12-28 22:05:16 +0100

    Correct unit tests for #24

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -13,6 +13,12 @@ if (PHP_SAPI !== 'cli') {
     echo 'Warning: Composer should be invoked via the CLI version of PHP, not the '.PHP_SAPI.' SAPI'.PHP_EOL;
 }
 
-if (!$loader = include __DIR__.'/../vendor/autoload.php') {
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+if (!$loader = include 'vendor/autoload.php') {
     die('You must set up the project dependencies.');
 }
