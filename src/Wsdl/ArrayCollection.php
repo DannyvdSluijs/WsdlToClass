100fd9f3b1496928a04ce308c2e0984facaf08a6
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2018-11-07 19:48:41 +0100

    Update copyright year

--- src/Wsdl/ArrayCollection.php
+++ src/Wsdl/ArrayCollection.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7.1
  *
- * @copyright 2015-2017 Danny van der Sluijs <danny.vandersluijs@icloud.com>
+ * @copyright 2015-2018 Danny van der Sluijs <danny.vandersluijs@icloud.com>
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU-GPL
  * @link      http://dannyvandersluijs.nl
  */
