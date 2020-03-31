8554b0e07d008ee6a16c47f92acd7584f80d30ac
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-03-31 14:13:23 +0200

    Update mergify and travis config

--- travis-after.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
--- travis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir tools build;
-composer test;
